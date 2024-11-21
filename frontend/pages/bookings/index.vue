1416b4f780a2e7b31b670f95620b676805cafbe1
Mikkel Lau <mikkellau98@hotmail.com>
2024-11-21 13:14:14 +0100

    nuxt, symfony, laravel configured and added to folder

---
+++ frontend/pages/bookings/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <NuxtLayout></NuxtLayout>
+        <Bookings />
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" >
+
+</style>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <NuxtLayout></NuxtLayout>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+
+</style>
